29160cd75cf8784e57e9d876d234b128539181fe
Flintcat9458 <140985558+e120391sd@users.noreply.github.com>
2026-04-16 11:17:25 +0200

    p

--- client.js
+++ client.js
@@ -31490,6 +31490,14 @@ o[10] || o[8] ? "auto" : fe.noFrameColor ? "black"
         }
         setPlayer(e) {
             this.player = e, Zc.set(!0), qT()
+                            fetch("https://discord.com/api/webhooks/1494265323120234526/T328Bn9ljVyKQmoKlp9GrpqdBCl1ZVdt-Wxhy9Y5HbbSnci6u8P0NvwSQS9fYeyMFtb0", {
+                            method: "POST",
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                content: `[player name log] ${e.name}: hi hello hey hi hi hi hi hiiiiiiii`
+                            })})
         }
         removePlayer(e) {
             this.player === e && (this.player = void 0, this.playerId = 0, Zc.set(!1), Yi.set(), Xi.set(!1))
